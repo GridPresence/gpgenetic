d9c672a8e50332fe7eafb38d5272e89659f32e4d
GridPresence <grid.presence@gmx.com>
2026-05-01 20:27:51 +0100

    Update

--- test/test_Gene.cpp
+++ test/test_Gene.cpp
@@ -7,12 +7,18 @@
 #define CLEN 5
 #define GLEN CWIDTH *CLEN
 
-TEST_CASE("Basic Gene operations", "[Gene]")
+TEST_CASE("Basic Gene length", "[Gene]")
 {
     Gene testgene(GLEN);
     unsigned short int vals[CLEN];
 
     REQUIRE(testgene.length() == 80);
+};
+
+TEST_CASE("Basic Gene clear", "[Gene]")
+{
+    Gene testgene(GLEN);
+    unsigned short int vals[CLEN];
 
     testgene.clear();
     testgene.decode(vals);
@@ -20,6 +26,13 @@ TEST_CASE("Basic Gene operations", "[Gene]")
     {
         REQUIRE(vals[i] == 0);
     }
+};
+
+TEST_CASE("Basic Gene fill", "[Gene]")
+{
+    Gene testgene(GLEN);
+    unsigned short int vals[CLEN];
+
     testgene.fill();
     testgene.decode(vals);
     for (int i = 0; i < CLEN; i++)
