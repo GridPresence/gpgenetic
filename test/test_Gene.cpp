c0d999beaf61cb495ce7aff603a3503cfe234ebf
GridPresence <grid.presence@gmx.com>
2026-05-01 18:44:40 +0100

    Let's go round again

--- test/test_Gene.cpp
+++ test/test_Gene.cpp
@@ -1,8 +1,9 @@
 #include <catch2/catch_test_macros.hpp>
 
-#include <Gene.hpp>
+#include "Gene.hpp"
 
-TEST_CASE("simple test case retrieving the correct length of the Gene","[Gene]") {
+TEST_CASE("simple test case retrieving the correct length of the Gene", "[Gene]")
+{
     Gene testgene(80);
 
     REQUIRE(testgene.length() == 80)
