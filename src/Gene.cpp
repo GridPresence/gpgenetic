5d0ae80c392b6be2f1af3f77f72d952ec9946fc5
GridPresence <grid.presence@gmx.com>
2026-05-02 20:12:54 +0100

    Bitty

--- src/Gene.cpp
+++ src/Gene.cpp
@@ -38,7 +38,7 @@ void Gene::decode(unsigned short int (&targ)[5])
     for (int i = 0; i < 5; i++)
     {
         word = 0;
-        for (int j = 15; j < 0; j--)
+        for (int j = 15; j >= 0; j--)
         {
             word = (word << 1) | m_dna[16 * i + j];
         }
