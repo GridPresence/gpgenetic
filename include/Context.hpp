dd96b600f93e03dfdff17391a31f9466b8196fcf
Jeremy Pavier <grid.presence@gmx.com>
2026-06-26 12:59:33 +0100

    Dev step: 2026-06-26_12-59-33

--- include/Context.hpp
+++ include/Context.hpp
@@ -1,14 +1,24 @@
+#include "rapidjson/document.h"
 #include <string>
 #include <map>
+#include <vector>
 
-using Response = std::map<int, double>;
+using Freqs = std::vector<double>;
+using Omega = std::vector<double>;
+using Bodes = std::vector<double>;
+using Response = std::vector<double>;
 
 class Context
 {
 private:
     int stages;
-    int sample_rate;
-    Response frequency_response;
+    double sample_rate;
+    Freqs frequencies;
+    Omega normalised_frequencies;
+    Bodes decibels;
+    Response linear_responses;
+
+    bool IsValid(rapidjson::Document &doc);
 
 public:
     Context(std::string &config);
--- src/Context.cpp
+++ src/Context.cpp
@@ -1,6 +1,52 @@
 #include <fstream>
+#include "rapidjson/filereadstream.h"
+#include <cstdio>
+#include <vector>
+
 #include "Context.hpp"
 
-Context::Context(std::string &config) {
+using namespace rapidjson;
 
+bool Context::IsValid(Document &doc)
+{
+    bool retval = true;
+    if (doc.HasMember("fsamp"))
+    {
+        retval = doc["fsamp"].IsNumber();
+    }
+    if (retval && doc.HasMember("stages"))
+    {
+        retval = doc["stages"].IsNumber();
+    }
+    if (retval && doc.HasMember("freqs"))
+    {
+    }
+    else
+    {
+        retval = false;
+    }
+    if (retval && doc.HasMember("bode"))
+    {
+    }
+    else
+    {
+        retval = false;
+    }
+
+    return retval;
+}
+
+Context::Context(std::string &config)
+{
+
+    FILE *fp = fopen(config.c_str(), "r");
+
+    char readBuffer[65536];
+    FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+
+    Document d;
+    d.ParseStream(is);
+    if (this->IsValid(d))
+    {
+    }
 };
