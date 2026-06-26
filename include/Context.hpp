61fd620287bbfab553af5c6973e9833bb7534b75
Jeremy Pavier <grid.presence@gmx.com>
2026-06-26 13:34:57 +0100

    Dev step: 2026-06-26_13-34-57

--- include/Context.hpp
+++ include/Context.hpp
@@ -1,4 +1,5 @@
 #include "rapidjson/document.h"
+#include <stdexcept>
 #include <string>
 #include <map>
 #include <vector>
@@ -22,4 +23,12 @@ private:
 
 public:
     Context(std::string &config);
+};
+
+
+// Base exception for our class
+class ContextException : public std::runtime_error {
+public:
+    ContextException(const std::string& message) 
+        : std::runtime_error(message) {}
 };
--- src/Context.cpp
+++ src/Context.cpp
@@ -13,32 +13,36 @@ bool Context::IsValid(Document &doc)
     if (doc.HasMember("fsamp"))
     {
         retval = doc["fsamp"].IsNumber();
+        if (!retval) throw ContextException("fsamp is not a Number");
     }
+    if (!retval) throw ContextException("fsamp is not present");
+
     if (retval && doc.HasMember("stages"))
     {
         retval = doc["stages"].IsNumber();
+        if (!retval) throw ContextException("stages is not a Number");
     }
+    if (!retval) throw ContextException("stages is not present");
+
     if (retval && doc.HasMember("freqs"))
     {
+        retval = doc["freqs"].IsArray();
+        if (!retval) throw ContextException("freqs is not an Array");
     }
-    else
-    {
-        retval = false;
-    }
+    if (!retval) throw ContextException("freqs is not present");
+
     if (retval && doc.HasMember("bode"))
     {
+        retval = doc["bode"].IsArray();
+        if (!retval) throw ContextException("bode is not an Array");
     }
-    else
-    {
-        retval = false;
-    }
+    if (!retval) throw ContextException("bode is not present");
 
     return retval;
 }
 
 Context::Context(std::string &config)
 {
-
     FILE *fp = fopen(config.c_str(), "r");
 
     char readBuffer[65536];
@@ -48,5 +52,17 @@ Context::Context(std::string &config)
     d.ParseStream(is);
     if (this->IsValid(d))
     {
+        for (SizeType i = 0; i < d["freqs"].Size(); i++)
+        {
+            frequencies.push_back(d["freqs"][i].GetDouble());
+        }
+        for (SizeType i = 0; i < d["bode"].Size(); i++)
+        {
+            decibels.push_back(d["bode"][i].GetDouble());
+        }
+        if (decibels.size() != frequencies.size())
+        {
+            throw ContextException("Unequal datapoints in bode specification");
+        }
     }
 };
