ff2de99b54cd56c8b72be180b348a2857a38295e
Jeremy Pavier <grid.presence@gmx.com>
2026-08-27 11:26:27 +0100

    Dev step: 2026-08-27_11-26-27

---
+++ app/evolve-iir.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#include "sarge/Sarge.h"
+#include "Context.hpp"
+
+int main(int argc, char* argv[])
+{
+    Sarge sarge;
+    sarge.setArgument("h", "help", "Get help.", false);
+	sarge.setArgument("i", "infile", "Input context file", true);
+    sarge.setArgument("o", "outfile", "Output file", true);
+    sarge.setUsage("sarge_test <options>");
+
+    if (!sarge.parseArguments(argc, argv)) {
+		std::cerr << "Couldn't parse arguments..." << std::endl;
+		return 1;
+	}
+	
+    if (sarge.exists("help")) {
+		sarge.printHelp();
+	}
+
+    std::string infile;
+	if (sarge.getFlag("infile", infile)) {
+		std::cout << "Got infile: " << infile << std::endl;
+	}
+
+    std::string outfile;
+	if (sarge.getFlag("outfile", outfile)) {
+		std::cout << "Got outfile: " << outfile << std::endl;
+	}
+
+	std::string textarg;
+	if (sarge.getTextArgument(0, textarg)) {
+		std::cout << "Got text argument: " << textarg << std::endl;
+	}
+
+    Context context(infile);
+
+    return 0;
+}
---
+++ app/sarge-test.cpp
@@ -0,0 +1,43 @@
+#include "sarge/Sarge.h"
+
+#include <iostream>
+
+
+int main(int argc, char** argv) {
+	Sarge sarge;
+	
+	sarge.setArgument("h", "help", "Get help.", false);
+	sarge.setArgument("k", "kittens", "K is for kittens. Everyone needs kittens in their life.", true);
+	sarge.setArgument("n", "number", "Gimme a number. Any number.", true);
+	sarge.setArgument("a", "apple", "Just an apple.", false);
+	sarge.setArgument("b", "bear", "Look, it's a bear.", false);
+	sarge.setArgument("", "snake", "Snakes only come in long form, there are no short snakes.", false);
+	sarge.setDescription("Sarge command line argument parsing testing app. For demonstration purposes and testing.");
+	sarge.setUsage("sarge_test <options>");
+	
+	if (!sarge.parseArguments(argc, argv)) {
+		std::cerr << "Couldn't parse arguments..." << std::endl;
+		return 1;
+	}
+	
+	std::cout << "Number of flags found: " << sarge.flagCount() << std::endl;
+	
+	if (sarge.exists("help")) {
+		sarge.printHelp();
+	}
+	else {
+		std::cout << "No help requested..." << std::endl;
+	}
+	
+	std::string kittens;
+	if (sarge.getFlag("kittens", kittens)) {
+		std::cout << "Got kittens: " << kittens << std::endl;
+	}
+
+	std::string textarg;
+	if (sarge.getTextArgument(0, textarg)) {
+		std::cout << "Got text argument: " << textarg << std::endl;
+	}
+	
+	return 0;
+}
